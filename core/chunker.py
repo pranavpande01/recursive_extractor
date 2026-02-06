865ba403a37797710b1d3636342b624da3180f93
pranavpande01 <93655463+pranavpande01@users.noreply.github.com>
2026-02-06 02:36:53 +0000

    first_commit

--- core/chunker.py
+++ core/chunker.py
@@ -1,1 +1,9 @@
-from chonkie import RecursiveChunker
+from chonkie import *
+
+recursive_chunker=RecursiveChunker()
+semantic_chunker=SemanticChunker()
+token_chunker=TokenChunker()
+sentence_chunker=SentenceChunker()
+late_chunker=LateChunker()
+slumber_chunker=SlumberChunker()
+chunks=recursive_chunker("")
